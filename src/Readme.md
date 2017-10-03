6dfdf66407e6a449de0621818aeb87ea5762a5ec
equity <nii.nmarley@gmail.com>
2017-10-03 09:44:53 +0000

    Initial Commit

---
+++ src/Readme.md
@@ -0,0 +1,6 @@
+# GDG UCC Hostel Discovery Application Backend
+
+- [ ] List hostels around campus
+- [ ] Find Detailed information about hostel
+- [ ] Admin dashboard to add hostel information
+- [ ] Users to post reviews/comments on hostels listed
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as logger from 'morgan';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import * as cors from 'cors';
+
+
+class Server {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    public config() {
+
+        const MONGO_URI = 'mongodb://localhost/hostels';
+        mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
+
+        this.app.use(bodyParser.urlencoded({ extended: true }));        
+        this.app.use(bodyParser.json());
+        this.app.use(helmet());
+        this.app.use(logger('dev'));
+        this.app.use(compression());
+        this.app.use(cors());
+    }
+
+    public routes(): void {
+
+    }
+
+}
