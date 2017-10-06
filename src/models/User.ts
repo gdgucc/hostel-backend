f7306fac6c3dc8fd06866a5fa97e594c078290d6
equity <nii.nmarley@gmail.com>
2017-10-06 09:31:02 +0000

    Fix

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,7 +12,7 @@ let UserSchema: Schema = new Schema ({
         required: true
     },
     password: {
-        type: 'String',
+        type: String,
         required: true
     },
     is_admin: {
