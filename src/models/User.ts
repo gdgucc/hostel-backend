e3aa3e4c288e101dff6a8e1f2355e73d0f2b6e49
equity <nii.nmarley@gmail.com>
2017-10-06 09:54:27 +0000

    Hash user passwords

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
 import { Schema, model } from 'mongoose';
+import { genSalt, hash } from 'bcrypt';
+
+let  SALT_WORK_FACTOR = 10;
 
 let UserSchema: Schema = new Schema ({
     createdAt: Date,
@@ -19,6 +22,27 @@ let UserSchema: Schema = new Schema ({
         type: Boolean,
         default: false
     }
+})
+
+UserSchema.pre('save', function(next){
+    var user = this;
+    
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 })
 
 export default model('User', UserSchema)
