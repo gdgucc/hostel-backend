6fb422dd2bd3c8edcf7377e60ff08963ec2c48a4
equity <nii.nmarley@gmail.com>
2017-10-06 09:29:54 +0000

    User Model

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+let UserSchema: Schema = new Schema ({
+    createdAt: Date,
+    updatedAt: Date,
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: 'String',
+        required: true
+    },
+    is_admin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+export default model('User', UserSchema)
